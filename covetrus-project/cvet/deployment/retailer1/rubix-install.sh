4d3d34e71281ba98a39b95a2bc9991d2dbd0a1cc
nitishkumar1288 <nitish.kumar1288@gmail.com>
2023-08-24 16:22:29 +0530

    test commit check

--- covetrus-project/cvet/deployment/retailer1/rubix-install.sh
+++ covetrus-project/cvet/deployment/retailer1/rubix-install.sh
@@ -61,43 +61,11 @@ echo "Got token for account ${DEV_ACCOUNT}"
 # Retrieve project version and name
 echo "Getting version and project name for account ${DEV_ACCOUNT}"
 
-echo "file=@artifacts"
-response=$( \
-  curl -v -s -o /dev/null -w "%{http_code}" -XPOST "${API_HOST}/orchestration/rest/v1/plugin/upload" \
-  -H "content-type: multipart/form-data" \
-  -H "Authorization: bearer ${token}" \
-  -H "Connection: keep-alive" \
-  -H 'Cache-Control: no-cache' \
-  -H "fluent.account: ${DEV_ACCOUNT}" \
-  -F "file=@./${GITHUB_WORKSPACE}/artifacts/${DEV_ACCOUNT}-1.0.15.jar"
-)
-if [[ ${response} -ne 200 ]]
-then
- abort_on_failure "Plugin Jar upload failed"
-fi
 
 # ######################
 # Step 5) Installing the plugin
 # ######################
 
-echo "Installing plugin  to account ${DEV_ACCOUNT} with namespace ${PLUGIN_NAME}"
-response=$( \
- curl -s -o /dev/null -w "%{http_code}" -XPOST "${API_HOST}/orchestration/rest/v1/plugin/install" \
- -H "Content-Type: application/json" \
- -H "Authorization: Bearer ${token}" \
- -H "Cache-Control: no-cache" \
- -H "fluent.account: ${DEV_ACCOUNT}" \
- -d "{\"accountId\":\"${DEV_ACCOUNT}\",\"bundleName\" :\"${DEV_ACCOUNT}.${PLUGIN_NAME}::1.0.15\"}"
-)
-echo "${response}"
-if [[ ${response} -ne 200 ]]
-then
- abort_on_failure "Plugin Jar install failed"
-fi
-echo "Plugin Install is Success With Code :: $response"
-
-echo "Sleeping for 10 seconds.........."
-sleep 10
 
 # ######################
 # Step 6) Verify plugin status
@@ -105,7 +73,7 @@ sleep 10
 
 resp=$( \
  curl -s -XGET \
- "${API_HOST}/orchestration/rest/v1/plugin/${DEV_ACCOUNT}.${PLUGIN_NAME}::1.0.15/status" \
+ "${API_HOST}/orchestration/rest/v1/plugin/${DEV_ACCOUNT}.${PLUGIN_NAME}::1.0.3/status" \
  -H "Authorization: Bearer ${token}" \
  -H "Cache-Control: no-cache" \
  | jq ".bundleVersion"
@@ -114,7 +82,7 @@ echo "Bundle Version :> $resp"
 
 stat=$( \
  curl -s -XGET \
- "${API_HOST}/orchestration/rest/v1/plugin/${DEV_ACCOUNT}.${PLUGIN_NAME}::1.0.15/status" \
+ "${API_HOST}/orchestration/rest/v1/plugin/${DEV_ACCOUNT}.${PLUGIN_NAME}::1.0.3/status" \
  -H "Authorization: Bearer ${token}" \
  -H "Cache-Control: no-cache" \
  | jq ".bundleStatus"
