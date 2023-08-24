529612b72bc4914688d9caf19e8dac3b549faf82
nitishkumar1288 <nitish.kumar1288@gmail.com>
2023-08-24 15:34:06 +0530

    test commit check

--- covetrus-project/cvet/deployment/retailer1/rubix-install.sh
+++ covetrus-project/cvet/deployment/retailer1/rubix-install.sh
@@ -10,7 +10,6 @@ do
   echo "$entry"
 done
 
-echo "current directory::" ${PATH}
 echo "DEV_ACCOUNT::" ${DEV_ACCOUNT}
 echo "API_HOST::" ${API_HOST}
 echo "RETAILER_PASSWORD::" ${RETAILER_PASSWORD}
@@ -69,7 +68,7 @@ response=$( \
   -H "Connection: keep-alive" \
   -H 'Cache-Control: no-cache' \
   -H "fluent.account: ${DEV_ACCOUNT}" \
-  -F "file=@artifacts/${DEV_ACCOUNT}-1.0.15.jar"
+  -F "file=@artifacts/${GITHUB_WORKSPACE}/artifacts/${DEV_ACCOUNT}-1.0.15.jar"
 )
 if [[ ${response} -ne 200 ]]
 then
