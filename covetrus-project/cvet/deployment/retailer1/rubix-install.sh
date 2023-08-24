deb9685f3a8e1a03bbe917987412485848fca760
nitishkumar1288 <nitish.kumar1288@gmail.com>
2023-08-24 10:41:33 +0530

    test commit check

--- covetrus-project/cvet/deployment/retailer1/rubix-install.sh
+++ covetrus-project/cvet/deployment/retailer1/rubix-install.sh
@@ -7,3 +7,9 @@ echo "Finding all files under directory first........"
 
 echo "DEV_ACCOUNT::" ${DEV_ACCOUNT}
 echo "API_HOST::" ${API_HOST}
+echo "RETAILER_PASSWORD::" ${RETAILER_PASSWORD}
+echo "PLUGIN_NAME::" ${PLUGIN_NAME}
+echo "RETAILER_USERNAME::" ${RETAILER_USERNAME}
+echo "CLIENT_SECRET::" ${CLIENT_SECRET}
+echo "GIT_EMAIL::" ${GIT_EMAIL}
+echo "GIT_USERNAME::" ${GIT_USERNAME}
